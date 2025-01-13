18cbcbfeeb807ad7c35f556bbe173df708e2dd9e
rootgh <95168267+Aliko2020@users.noreply.github.com>
2025-01-13 03:26:48 -1200

    navbar animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,65 +1,75 @@
 import { Link } from "react-router-dom";
 import workspace from "../images/workspace.jpeg";
-import { BiSolidCoffeeAlt } from "react-icons/bi";
 import { FaExternalLinkAlt } from "react-icons/fa";
-
-
+import Typewriter from "../components/Typewritter";
 
 const Home = () => {
   return (
-    <main className="p-4">
-      <section className="hero ">
-        <div className="max-w-2xl leading-relaxed">
-          <div className="">
-          <span className="hero-text text-3xl">Hello,</span>
-          <br />
-          <h1 className="hero-text text-3xl">
-            I’m Amos,Web Developer,
-          </h1>
-          <p className="mt-2 ">
-            I'm Aliko, a web developer with over two years of experience and a
-            strong foundation in computer science. I'm passionate about finding
-            creative solutions to challenges and sharing them with the world
-            through the internet. I'm always eager to connect with other
-            developers and learn from their experiences
-          </p>
-          <button className="btn-connect current-link">
-            <BiSolidCoffeeAlt size={30} />
-            Let's Contact
-          </button>
+    <main className="flex flex-col p-4">
+        {/* hero text and image start */}
+        <section className="hero flex-grow flex items-center">
+          <div className="max-w-2xl leading-relaxed">
+            <div>
+              <span className="text-3xl p-1">
+                <Typewriter text="H i, " />
+              </span>
+              <br />
+              <h1 className="text-3xl p-1">
+                <Typewriter text="I'm Amos, a Web Developer," />
+              </h1>
+              <p className="mt-2">
+                with over two years of experience and a strong foundation in
+                computer science. I'm passionate about finding creative
+                solutions to challenges and sharing them with the world through
+                the internet. I'm always eager to connect with other developers
+                and learn from their experiences.
+              </p>
+              <button
+                className="btn-connect current-link"
+                onClick={() =>
+                  (window.location.href = "mailto:your-email@example.com")
+                }
+                aria-label="connect"
+              >
+                Let's Connect
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="hidden lg:block">
-          <img
-            src={workspace}
-            className="max-w-md"
-            alt="work space illustration"
-          />
-        </div>
-      </section>
-      <div className="flex justify-between items-center gap-8">
-        <div className="flex gap-4">
-          <a
-            href="https://www.linkedin.com/in/aliko-amos-0a7098246/"
-            target="_blank"
-            className="flex border-r-2 border-standout pr-4 links"
-          >
-            LinkedIn{" "}
-            <FaExternalLinkAlt size={10}/>
-          </a>
-          <a
-            href="https://github.com/Aliko2020"
-            target="_blank"
-            className="flex links"
-          >
-            GitHub{" "}
-            <FaExternalLinkAlt size={10}/>
-          </a>
-        </div>
-        <Link className="links" to="/">
-          HIRE ME
-        </Link>
-      </div>
+          <div className="hidden lg:block">
+            <img
+              src={workspace}
+              className="max-w-md"
+              alt="Illustration of a workspace with a laptop and coffee"
+            />
+          </div>
+        </section>
+        {/* hero text and image end */}
+        {/* footer starts here */}
+        <section className="flex justify-between items-center gap-8">
+          <div className="flex gap-4">
+            <a
+              href="https://www.linkedin.com/in/aliko-amos-0a7098246/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex border-r-2 border-standout pr-4 links"
+              aria-label="Visit my LinkedIn profile"
+            >
+              LinkedIn <FaExternalLinkAlt size={10} />
+            </a>
+            <a
+              href="https://github.com/Aliko2020"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex links"
+              aria-label="Visit my GitHub profile"
+            >
+              GitHub <FaExternalLinkAlt size={10} />
+            </a>
+          </div>
+          <Link className="links" to="/" aria-label="Hire me">
+            Resume
+          </Link>
+        </section>
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,28 @@ import { CiMail } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <header className="flex justify-center lg:justify-end items-center pt-4 px-12">
-        <nav className="p-2 bg-[#f2f2f0] rounded-lg dark:text-darkbg shadow-md fixed">
-          <ul className="flex gap-8 font-semibold ">
+    <header className="flex justify-center lg:justify-end items-center pt-2 lg:pt-8 px-4 mb-2">
+      <nav className="p-2 rounded-lg shadow-md lg:fixed text-white bg-[#5b5e62] relative">
+        <div className="absolute inset-0 -z-10 rounded-lg bg-gradient-to-r from-orange-500 via-red-400 to-pink-500 p-[1px]">
+          <div className="bg-[#5b5e62] h-full w-full rounded-lg"></div>
+        </div>
+        <ul className="flex gap-8 font-semibold">
           <Link className="links current-link" to="/">
-              <CiHome size={24} />
+            <CiHome size={24} />
           </Link>
-            <Link className="links current-link" to="/about">
-              <CiUser size={24}/>
-            </Link>
-            <Link className="links current-link" to="/projects">
-              <CiFolderOn size={25}/>
-            </Link>
-            <Link className="links current-link" to="/">
-              <CiMail size={24} />
-            </Link>
-          </ul>
-        </nav>
-      </header>
-  )
-}
+          <Link className="links current-link" to="/about">
+            <CiUser size={24} />
+          </Link>
+          <Link className="links current-link" to="/projects">
+            <CiFolderOn size={25} />
+          </Link>
+          <Link className="links current-link" to="/">
+            <CiMail size={24} />
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
